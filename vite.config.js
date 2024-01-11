9bfb6f92b78993b62c21ecfe877471347f48e769
mohamad95 <algoolm421@gmail.com>
2024-01-11 07:37:47 -0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base :"LINKEDIN-CLONE",
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 10000, // Set your preferred size limit
-  }
+
 })
